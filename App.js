7d9576684acf960e6c38a3dd76178b3107412928
ado durak <76266634+Alginest@users.noreply.github.com>
2021-05-28 15:04:16 +0200

    Add files via upload

---
+++ App.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Setup from './tutorial/6-useReducer/setup'
+// import Final from './tutorial/1-useState/final/1-cont'
+
+function App() {
+  return (
+    <div className='container'>
+      <Setup />
+    </div>
+  )
+}
+
+export default App
